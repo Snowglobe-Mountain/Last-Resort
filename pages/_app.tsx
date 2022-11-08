11e3cc1cb1b9d0e81907cdf2771165cf4fc6cccd
CovenEsme <84377742+CovenEsme@users.noreply.github.com>
2022-11-08 23:22:34 +0000

    Fully migrate to use tailwindcss

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,11 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from 'next-themes'
 
 const App: JSX.Element = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
+  return (
+    <Component {...pageProps} />
+  )
 }
 
 export default App
